58ad5b9c094d21a861b74e3e553d6407d30d8d46
TreyBrowder <tbrowder7@ymail.com>
2022-04-25 19:57:02 -0400

    added in more photos for dummy data - still working on getting pics to populate correctly

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import { Grid } from '@material-ui/core';
-
 import Product from './Product/Product'
+import React from 'react';
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://cdn.runrepeat.com/storage/gallery/product_primary/37483/adidas-adizero-adios-pro-20-hero-2-15915079-960.jpg'},
-    {id: 2, name: 'MacBook', description: 'Apple MacBook Computer', price: '$10', image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fmacbook-pro&psig=AOvVaw2JSnLT389sIxcqAvuqHOs7&ust=1649273203674000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCMiwz83T_fYCFQAAAAAdAAAAABAD'},
+    {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://lh3.googleusercontent.com/-kfptSZIKctA/YVMkBifRLNI/AAAAAAACPJ4/dcKSTTAz6GohZFRr1rQwj_akt1CmCElkgCNcBGAsYHQ/w547-h410/IMG_2396.jpg},
+    {id: 2, name: 'MacBook', description: 'Apple MacBook Computer', price: '$10', image: 'https://photos5.appleinsider.com/gallery/26909-39215-macbook-pro-13-inch-touch-bar-review00017-xl.jpg'},
 ];
 
 const Products = () => {
     return (
+        
         <main> 
         <Grid container justify="center" spacing={4}>
              {products.map((product) => (
