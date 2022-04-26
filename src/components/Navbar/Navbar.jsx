e612599c32feca61791a837829cfc2287b7b6183
TreyBrowder <tbrowder7@ymail.com>
2022-04-25 20:17:18 -0400

    created NavBar component and started importing icons needed for Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import { AppBar, Badge, IconButton, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core'
+
+import React from 'react'
+import { ShoppingCart } from '@material-ui/core'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
