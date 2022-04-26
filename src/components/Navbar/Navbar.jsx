926c0e39d246ad188267e2907616ade7775c80f5
TreyBrowder <tbrowder7@ymail.com>
2022-04-25 21:25:45 -0400

    worked on navbar component and added assets folder to house images needed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
-import { AppBar, Badge, IconButton, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Badge, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography } from '@material-ui/core'
 
 import React from 'react'
 import { ShoppingCart } from '@material-ui/core'
+import eCommerceLogo from '../../assets/eCommercePic.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <AppBar position="fixed" className={classes.appBar} color="inherit">
+            <Toolbar>
+                <Typography variant="h6" className={classes.title} color="inherit">
+                    <img src={eCommerceLogo} alt="Commerce.js" height="25px" className={classes.image}/>
+                    Commerce.js
+                </Typography>
+                <div className={classes.grow} />
+                <div className={classes.button}>
+                    <IconButton aria-label="Show cart items" color="inherit">
+                        <Badge badgeContent={2} color="secondary"></Badge>
+                    </IconButton>
+                </div>
+            </Toolbar>
+        </AppBar>
+    </div>
   )
 }
 
